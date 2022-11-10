5cf4ed20040c0de1e5288d0bfeb2e2dd036c7157
Yeferson <131elyefer@gmail.com>
2022-11-10 06:52:57 -0500

    primer final

--- server/index.js
+++ server/index.js
@@ -11,17 +11,15 @@ const server = http.createServer(app);
 const io = new SocketServer(server, {
     maxHttpBufferSize: 1000e8,
     cors: {
-      origin: ["https://gleaming-stroopwafel-b44634.netlify.app"],
+      origin: ["https://react-fron-chat.vercel.app/"],
       allowedHeaders: ["my-custom-header"],
       credentials: true
     }
 });
 
-const options = { origin: "https://gleaming-stroopwafel-b44634.netlify.app", }
+const options = { origin: "https://react-fron-chat.vercel.app/" }
 app.use(cors(options))
-
 app.use(morgan('dev'));
-
 
 io.on('connection', (socket) => {
 
@@ -37,18 +35,6 @@ io.on('connection', (socket) => {
         console.log("messageText", messageObject)
     })
 
-    // socket.on("messageFile", (messageFile) => {
-    //     socket.broadcast.emit('messageFile', {
-    //         user_id: socket.id,
-    //         body: messageFile.file,
-    //         type: messageFile.type,
-    //         name: socket.name,
-    //         size: socket.size,
-    //         from: "User"
-    //     })
-    //     console.log("messageFile", messageFile);
-    // });
-    
 })
 
 // starting the server
